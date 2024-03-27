acdf5784668bd52631d3ce14f23bb1dcf239ceee
Amy Collins <amyazure06@gmail.com>
2024-03-27 23:58:42 +0000

    Create finddupe.py

---
+++ finddupe.py
@@ -0,0 +1,14 @@
+import subprocess
+
+filenames = []
+
+proc = subprocess.run("find", capture_output=True, text=True)
+files = proc.stdout.split("\n")
+for i in files:
+    tmparr = i.split("/")
+    filename = tmparr[-1]
+    if filename != "" or filename != " ":
+        if filename in filenames:
+            print(i)
+        else:
+            filenames.append(filename)
